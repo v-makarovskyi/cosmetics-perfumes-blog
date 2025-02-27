8d9be46c58ee4857418106011d7223c6ea65ea27
Volodymyr Makarovskyi <makarovskyi.v@gmail.com>
2025-02-27 14:04:20 +0200

    added styles for forgot in __login.scss. Added LoginForm and validation.

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,6 @@ function App() {
   return (
     <div className="app">
       <LoginPage />
-      <Footer />
     </div>
   );
 }
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,15 +1,17 @@
-import React, { FC } from 'react'
-import { Wrapper } from '../layout/wrapper'
-import { Header } from '../layout/header/header'
-import { CommonBreadcrumb } from '../components/breadcrumb/common-breadcrumb'
-import { LoginArea } from '../components/login-register/login-area'
+import React, { FC } from "react";
+import { Wrapper } from "../layout/wrapper";
+import { Header } from "../layout/header/header";
+import { CommonBreadcrumb } from "../components/breadcrumb/common-breadcrumb";
+import { LoginArea } from "../components/login-register/login-area";
+import { Footer } from "../layout/footer/footer";
 
-export const LoginPage:FC = ():JSX.Element => {
+export const LoginPage: FC = (): JSX.Element => {
   return (
     <Wrapper>
-        <Header />
-        <CommonBreadcrumb title='Войти' subtitle='Войти' center={true} />
-        <LoginArea />
+      <Header />
+      <CommonBreadcrumb title="Войти" subtitle="Войти" center={true} />
+      <LoginArea />
+      <Footer />
     </Wrapper>
-  )
-}
+  );
+};
