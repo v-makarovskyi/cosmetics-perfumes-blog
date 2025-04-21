a1a69671a4f7a0f8649add7adc722263832dbf1d
Volodymyr Makarovskyi <makarovskyi.v@gmail.com>
2025-04-21 15:50:39 +0300

    custom error Auth

---
+++ errors/auth-error.js
@@ -0,0 +1,13 @@
+class AuthError extends Error {
+  constructor(statusCode='', message) {
+    super(message);
+    this.statusCode = statusCode;
+    this.name = "AuthError";
+
+    if ("captureStackTrace" in Error) {
+      Error.captureStackTrace(this, AuthError);
+    }
+  }
+}
+
+module.exports = AuthError;
