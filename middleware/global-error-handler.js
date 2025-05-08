24a3c1e66443459ea3e843bed1668e7941dad761
Volodymyr Makarovskyi <makarovskyi.v@gmail.com>
2025-05-08 13:29:53 +0300

    update errors handler

--- middleware/global-error-handler.js
+++ middleware/global-error-handler.js
@@ -1,6 +1,7 @@
 const ServerFieldValidationError = require("../errors/server-validation-error");
 const AuthError = require("../errors/auth-error");
 const { Result } = require("express-validator");
+const jwt = require("jsonwebtoken");
 
 const globalErrorHandler = (error, req, res, next) => {
   let statusCode = 500;
@@ -8,9 +9,27 @@ const globalErrorHandler = (error, req, res, next) => {
   let errorMessage = error.message;
   let errorMessagesArray = [];
 
-  if (error.validationErrors instanceof Result) {
+  if (error.name === "TokenExpiredError") {
+    let queryString;
+    let params = req.body["targetUrl"].split("/").filter(Boolean);
+    let paramsSlicedArray = [];
+    const SIZE = 2;
+    for (let i = 0; i < params.length; i += SIZE) {
+      paramsSlicedArray.push(params.slice(i, i + SIZE));
+    }
+
+    params = Object.fromEntries(paramsSlicedArray);
+    queryString = Object.keys(params)
+      .map((key) => key + "=" + params[key])
+      .join("&");
+
+    res.redirect(`/api/auth/refresh_token?${queryString}`);
+    return;
+  } else if (error.validationErrors instanceof Result) {
     error = new ServerFieldValidationError(error);
+    errorName = error.name;
     statusCode = error.statusCode;
+    errorMessage = error.message;
     errorMessagesArray = error.errorsDetail;
   } else if (error instanceof AuthError) {
     statusCode = error.statusCode;
@@ -21,14 +40,13 @@ const globalErrorHandler = (error, req, res, next) => {
       },
     ];
   } else if (error instanceof Error) {
-    
     errorMessagesArray = [
       {
         location: req.originalUrl,
         message: `Ошибка в ${req.originalUrl}`,
       },
     ];
-  } 
+  }
 
   res.status(statusCode).json({
     success: false,
@@ -39,5 +57,4 @@ const globalErrorHandler = (error, req, res, next) => {
     stack: req.app.get("env") === "development" ? error?.stack : {},
   });
 };
-
 module.exports = globalErrorHandler;
