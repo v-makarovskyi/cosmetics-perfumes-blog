5e6d57000893c530e8eea741ad177991dcc8de6b
Volodymyr Makarovskyi <makarovskyi.v@gmail.com>
2025-05-08 13:36:19 +0300

    added ToastContainer

--- client/src/layout/wrapper.tsx
+++ client/src/layout/wrapper.tsx
@@ -1,9 +1,15 @@
 import React, { FC } from "react";
+import { ToastContainer } from "react-toastify";
 
 type WrapperProps = {
   children: React.ReactNode;
 };
 
 export const Wrapper: FC<WrapperProps> = ({ children }): JSX.Element => {
-  return <div id="wrapper">{children}</div>;
+  return (
+     <div id="wrapper">
+      {children}
+      <ToastContainer />
+    </div>
+  )
 };
