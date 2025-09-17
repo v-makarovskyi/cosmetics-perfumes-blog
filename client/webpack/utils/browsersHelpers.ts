f180526438690c12411d103ac41ba95891654ecb
Volodymyr Makarovskyi <makarovskyi.v@gmail.com>
2025-09-17 18:17:07 +0300

    added logic chek suporrted browsers

---
+++ client/webpack/utils/browsersHelpers.ts
@@ -0,0 +1,86 @@
+//hode.js core
+import path from "path";
+import fs from "fs";
+import os from "os";
+import { readFile } from "fs/promises";
+
+//import node_modules packages
+import { packageUp } from "package-up";
+import browserslist from "browserslist";
+import prompts from "prompts";
+import chalk from "chalk";
+//import yargs from 'yargs';
+
+//import types
+import type { PromptsObjectType } from "../webpack.types";
+import { appPaths } from "../config/paths";
+
+const defaultBrowsers: { [x: string]: readonly string[] } = {
+  production: [">0.2%", "not dead", "not op_mini all"],
+  development: [
+    "last 1 chrome version",
+    "last 1 safari version",
+    "last 1 firefox version",
+  ],
+};
+
+function shouldSetBrowsers(isInteractive: boolean) {
+  if (!isInteractive) return Promise.resolve(true);
+  const question: PromptsObjectType = {
+    type: "confirm",
+    name: "shouldSetBrowsers",
+    message:
+      chalk.yellow("Мы не можем определить целевые браузеры.") +
+      `\n\nХотите ли вы добавить список браузеров по умолчанию в свой ${chalk.red(
+        "package.json"
+      )} файл?`,
+    initial: true,
+  };
+  return prompts(question).then((answer) => answer.shouldSetBrowsers);
+}
+
+function checkBrowsers(
+  dir: string,
+  isInteractive: boolean,
+  retry: boolean = true
+) {
+  const current = browserslist.loadConfig({ path: dir });
+  if (current !== void 0) return Promise.resolve(current);
+
+  if (!retry) {
+    return Promise.reject(
+      new Error(
+        chalk.red(
+          `Список браузеров по умолчанию в ${chalk.bold(
+            "package.json"
+          )} не установлен.` +
+            `\nПожалуйста добавьте поле ${chalk.underline(
+              "browserslist"
+            )} в ваш ${chalk.bold("package.json")}.`
+        )
+      )
+    );
+  }
+
+  return shouldSetBrowsers(isInteractive).then((shouldSetBrowsers) => {
+    if (!shouldSetBrowsers) return checkBrowsers(dir, isInteractive, false);
+    return packageUp({ cwd: dir }).then((filepath) => {
+      if (filepath === void 0) return Promise.reject();
+      const pkg = JSON.parse(fs.readFileSync(filepath, "utf-8"));
+      pkg["browserslist"] = defaultBrowsers;
+      fs.writeFileSync(filepath, JSON.stringify(pkg, null, 2) + os.EOL);
+      browserslist.clearCaches();
+      console.log();
+      console.log(
+        chalk.blue(
+          `Дефолтный список браузеров успешно установлен. Проверьте свой ${chalk.bold.underline(
+            "package.json"
+          )}.`
+        )
+      );
+      console.log();
+    });
+  });
+}
+
+export { checkBrowsers, defaultBrowsers };
