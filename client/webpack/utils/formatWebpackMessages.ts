605019c533bde5c5c5c4a9dfb34de385f73d942c
Volodymyr Makarovskyi <makarovskyi.v@gmail.com>
2025-09-26 19:20:00 +0300

    updated

--- client/webpack/utils/formatWebpackMessages.ts
+++ client/webpack/utils/formatWebpackMessages.ts
@@ -11,25 +11,35 @@ function formatMessage(messageObj: AppStatsErrorOrWarning) {
   let { moduleIdentifier, moduleName, loc, message } = messageObj;
   let lines: any = [moduleIdentifier, moduleName, loc, message];
 
-
   if (lines && lines[3] && typeof lines[3] === "string") {
     lines[3] = lines[3].split("\n");
-    if(lines[3].length > 1) {
-      lines[3][1] = chalk.bold.yellow(lines[3][1])
+    if (lines[3].length > 1) {
+      lines[3][1] = chalk.bold.yellow(lines[3][1]);
     }
     if (lines && lines[3] && Array.isArray(lines[3])) {
       lines[3] = lines[3].map((line) => {
         if (line.indexOf("Module not found") === 0) {
-          let colorLinePath = line.match(/\/home\/.+/g)
-          line = line.replace(
-            "Module not found: Error: Can't resolve",
-            chalk.bold("NOT FOUND OR RESOLVE FILE: ")
-          ).replace(colorLinePath, chalk.cyan.bold(colorLinePath))
+          let colorLinePath = line.match(/\/home\/.+/g);
+          line = line
+            .replace(
+              "Module not found: Error: Can't resolve",
+              chalk.bold("NOT FOUND OR RESOLVE FILE: ")
+            )
+            .replace(colorLinePath, chalk.cyan.bold(colorLinePath));
           return line;
         }
         if (line.indexOf("Module parse failed") === 0) {
-          line = line.replace("Module parse failed", chalk.bold("PARSING ERROR in MODULE"));
+          line = line.replace(
+            "Module parse failed",
+            chalk.bold("PARSING ERROR in MODULE")
+          );
           return line;
+        }
+        if (line.indexOf("Module build failed") === 0) {
+          line = line.replace(
+            "Module build failed",
+            chalk.bold("MODULE BUILD FAILED:")
+          );
         }
         return line;
       });
@@ -37,11 +47,13 @@ function formatMessage(messageObj: AppStatsErrorOrWarning) {
   }
   output += `${chalk.red.bold("Something went wrong")} ${chalk.bold.blue(
     "in"
-  )} ${chalk.blueBright(lines[1])} ${chalk.bold.greenBright(lines[2])}\n\n`;
-  
-  output += `${lines[3].join('\n')}\n`
-  output+= '-------------------------------------------------------'
-  return output
+  )} ${chalk.blueBright(lines[1])} ${chalk.bold.greenBright(
+    lines[2] ? lines[2] : ""
+  )}\n\n`;
+
+  output += `${lines[3].join("\n")}\n`;
+  output += "-------------------------------------------------------";
+  return output;
 }
 
 function formatWebpackMessages(json: AppStatsCompilations) {
