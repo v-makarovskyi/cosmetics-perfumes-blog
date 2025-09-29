8aa39251adb35cf1d70174475cc9825c5b5032ee
Volodymyr Makarovskyi <makarovskyi.v@gmail.com>
2025-09-30 01:40:51 +0300

    get ip address function

---
+++ client/webpack/utils/getIp.ts
@@ -0,0 +1,13 @@
+import os from "os";
+
+function getIp() {
+  const interfaces = os.networkInterfaces();
+  const ip = Object.values(interfaces)
+    .flat()
+    .filter((val) => val?.family === "IPv4" && !val.internal)
+    .map((el) => el?.address)
+    .join('')
+
+  getIp()
+}
+
