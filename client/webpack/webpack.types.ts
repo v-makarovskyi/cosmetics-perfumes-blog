626595629ce1214ec9abd8a7ce7af2f5653b4e72
Volodymyr Makarovskyi <makarovskyi.v@gmail.com>
2025-10-09 12:43:34 +0300

    delete

--- client/webpack/webpack.types.ts
+++
@@ -1,9 +0,0 @@
-export type WebpackConfiguration = import("webpack").Configuration;
-export type WebpackConfigEnv = "development" | "production"
-
-//type for prompts object. Using in 'utils/browsersHelpers.ts'
-export type PromptsObjectType = import('prompts').PromptObject
-
-export interface AppPaths {
-    [x: string]: string | string[]
-}
