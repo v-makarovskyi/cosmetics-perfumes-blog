9d8067e7a5ebf982afce1199b47e428c170b4973
Volodymyr Makarovskyi <makarovskyi.v@gmail.com>
2025-12-27 10:37:49 +0200

    update

--- app.js
+++ app.js
@@ -23,18 +23,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(logger("dev"));
 app.use(
   cors({
-    origin: function (origin, cb) {
-      if (
-        [
-          "http://localhost:9000",
-          "https://cosmetics-perfumes-blog.vercel.app",
-        ].indexOf(origin) !== -1
-      ) {
-        cb(null, true);
-      } else {
-        cb(new Error("Не разрешено CORS"));
-      }
-    },
+    origin: "https://cosmetics-perfumes-blog.vercel.app",
     credentials: true,
     optionsSuccessStatus: 200,
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
