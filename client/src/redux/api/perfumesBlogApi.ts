07ce7a843b6142fd042eea9318b2a17ae1db8b8e
Volodymyr Makarovskyi <makarovskyi.v@gmail.com>
2025-12-28 16:34:50 +0200

    updated baseUrl

--- client/src/redux/api/perfumesBlogApi.ts
+++ client/src/redux/api/perfumesBlogApi.ts
@@ -1,11 +1,11 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 const PUBLIC_API_BASE_URL = 'https://cosmetics-perfumes-blog.vercel.app/api/'
-
+const  PUBLIC_API_BASE_URL_DEV = 'http://localhost:9000/api/'
 
 export const perfumesBlogApi = createApi({
   reducerPath: "perfumesBlogApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: PUBLIC_API_BASE_URL,
+    baseUrl: process.env.NODE_ENV === 'development' ? PUBLIC_API_BASE_URL_DEV : PUBLIC_API_BASE_URL,
     prepareHeaders: (headers,  {getState}) => {},
     credentials: "include",
   }),
